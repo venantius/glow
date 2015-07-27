d67ba9d6565de515a17f058f9ce00b8d4bf4c8e7
Ursa Americanus Kermodei <venantius@gmail.com>
2015-07-27 17:32:27 -0400

    [regex] Initial matching expressions for strings and regex literals

--- src/searchlight/core.clj
+++ src/searchlight/core.clj
@@ -1,6 +1,18 @@
 (ns searchlight.core)
 
-(defn match-string
-  "Parse a string of source code and try to match on strings"
+;; match-comment
+;; match-keyword
+;; match-number
+
+;; match-vector?
+;; match-parens
+
+(defn match-regex
+  "Parse a string of source code and try to match on regex literals."
   [s]
-  (re-find #"(?:\"[^\"].*\")" s))
+  (re-find #"(?s:#\"[^\"].*\")" s))
+
+(defn match-string
+  "Parse a string of source code and try to match on string literals."
+  [s]
+  (re-find #"(?s:\"[^\"].*\")" s))
--- test/searchlight/core_test.clj
+++ test/searchlight/core_test.clj
@@ -1,11 +1,20 @@
 (ns searchlight.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [searchlight.core :as core]))
 
+(deftest match-regex-works
+  (let [a (slurp (io/resource "test/regex/regexes/example_1.txt"))
+        b (slurp (io/resource "test/regex/regexes/example_2.txt"))]
+    (is (= "#\"\\\"\"" (core/match-regex a)))
+    (is (= "#\"(regex)\"" (core/match-regex b)))))
+
 (deftest match-string-works
-  (let [a (slurp "test/example")
-        b (slurp "test/example2")
-        c (slurp "test/example3")]
+  (let [a (slurp (io/resource "test/regex/strings/example"))
+        b (slurp (io/resource "test/regex/strings/example2"))
+        c (slurp (io/resource "test/regex/strings/example3"))
+        d (slurp (io/resource "test/regex/strings/example4"))]
     (is (= "\"test\"" (core/match-string a)))
     (is (nil? (core/match-string b)))
-    (is (= "\"test \\\" asdf \"" (core/match-string c)))))
+    (is (= "\"test \\\" asdf \"" (core/match-string c)))
+    (is (= "\"will be an\nexample\"" (core/match-string d)))))
