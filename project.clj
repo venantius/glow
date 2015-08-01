03b7198fe2ebddb7a4448fac7bdcaf9ece0770aa
Ursa Americanus Kermodei <venantius@gmail.com>
2015-08-01 21:09:02 +0100

    [release] 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject venantius/glow "0.1.0-SNAPSHOT"
+(defproject venantius/glow "0.1.0"
   :description "Syntax highlighting for Clojure source code."
   :url "https://github.com/venantius/glow"
   :license {:name "Eclipse Public License"
