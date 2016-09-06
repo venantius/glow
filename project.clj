eba9db6ee4fa6c9578914a1408a1d2615ad4ba3b
Ursa Americanus Kermodei <venantius@gmail.com>
2016-09-06 15:26:58 +0100

    [glow] Upgrade clj-antler to 0.2.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject venantius/glow "0.1.3"
+(defproject venantius/glow "0.1.4"
   :description "Syntax highlighting for Clojure source code."
   :url "https://github.com/venantius/glow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-antlr "0.2.2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-antlr "0.2.3"]
                  [garden "1.1.7"]
                  [hiccup "1.0.5"]
                  [instaparse "1.4.1"]])
